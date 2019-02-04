d8eea1ea1a2845d79ac71be70f4f584fdec7f9da
luca <lucadid3503@aol.com>
2019-02-04 22:07:31 +0100

    first

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Questa è la prima app mobile</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color:'red',
+    fontSize: 25,
+  },
+});
